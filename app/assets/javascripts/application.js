c441a4e55224ec505fb28926799977d959cbd065
Nayana Gharte <nayana@weboniselab.com>
2012-11-21 17:25:37 +0530

    Apply bootstrap & resolve issues related to it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,48 +12,72 @@
 //
 //= require jquery
 //= require jquery_ujs
+
 //= require_tree .
 
 
+var actual_count=30
+if ($('#content').children().size() < actual_count){
+    $(document).ready(function () {
+
+        $(function(){
+
+            $('#content').scrollPagination({
+
+                'contentPage': 'sample/show_data', // the url you are fetching the results
+                'contentData': {}, // these are the variables you can pass to the request, for example: children().size() to know which page you are
+                'scrollTarget': $(window), // who gonna scroll? in this example, the full window
+                'heightOffset': 1, // it gonna request when scroll is 10 pixels before the page ends
+
+                'beforeLoad': function(){ // before load function, you can display a preloader div
+
+                    $('#loading').fadeIn();
 
 
-$(document).ready(function () {
 
-    $(function(){
-        $('#content').scrollPagination({
+                },
+                'afterLoad': function(elementsLoaded){ // after loading content, you can use this function to animate your new elements
+                    $('#loading').fadeOut();
+                    var i = 0;
+                    $(elementsLoaded).fadeInWithDelay();
 
-            'contentPage': 'sample/show_data', // the url you are fetching the results
-            'contentData': {}, // these are the variables you can pass to the request, for example: children().size() to know which page you are
-            'scrollTarget': $(window), // who gonna scroll? in this example, the full window
-            'heightOffset': 10, // it gonna request when scroll is 10 pixels before the page ends
-            'beforeLoad': function(){ // before load function, you can display a preloader div
-                $('#loading').fadeIn();
-            },
-            'afterLoad': function(elementsLoaded){ // after loading content, you can use this function to animate your new elements
-                $('#loading').fadeOut();
-                var i = 0;
-                $(elementsLoaded).fadeInWithDelay();
-                if ($('#content').children().size() > 50){ // if more than 100 results already loaded, then stop pagination (only for testing)
-                    $('#nomoreresults').fadeIn();
-                    $('#content').stopScrollPagination();
+
+                    if ($('#content').children().size() > actual_count){ // if more than 100 results already loaded, then stop pagination (only for testing)
+                        $('#nomoreresults').fadeIn();
+                        $('#content').stopScrollPagination();
+                    }
+
                 }
-            }
-        });
-
-        $("img.lazy").lazyload({
-            effect : "fadeIn"  ,
-            event: "scrollstop"
-        });
-        // code for fade in element by element
-        $.fn.fadeInWithDelay = function(){
-            var delay = 0;
-            return this.each(function(){
-                $(this).delay(delay).animate({opacity:1}, 200);
-                delay += 100;
             });
-        };
+
+
+            $("img.lazy").lazyload({
+                effect : "fadeIn"  ,
+                event: "scrollstop"
+            });
+            // code for fade in element by element
+            $.fn.fadeInWithDelay = function(){
+                var delay = 0;
+                return this.each(function(){
+                    $(this).delay(delay).animate({opacity:1}, 200);
+                    delay += 100;
+                });
+            };
+
+        });
 
     });
+}
+else
+{
+    $("img.lazy").lazyload({
+        effect : "fadeIn"  ,
+        event: "scrollstop"
+    });
+    $('#content').stopScrollPagination();
+    $("img.lazy").lazyload({
+        effect : "fadeIn"  ,
+        event: "scrollstop"
+    });
+}
 
-
-});
